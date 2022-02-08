de7ed67dcd8ec03d65da3b1301914948a45c899b
danigarcia47 <dgarcial01@educantabria.es>
2022-02-08 12:18:12 +0100

    carpeta src

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
-import "./style.css";
+import logo from './logo.svg';
+import './App.css';
 
-export default function App() {
+function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          EQUIPO1
+        </a>
+      </header>
     </div>
   );
 }
+
+export default App;
